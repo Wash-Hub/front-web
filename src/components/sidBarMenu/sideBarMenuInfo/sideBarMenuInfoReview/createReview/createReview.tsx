55788a9823a7b434be88be4f14408db66c908c63
ohamin26 <ohamin26@gmail.com>
2024-06-29 23:07:58 +0900

    ✨ feat : 리뷰 등록 페이지 이미지 미리보기 기능 추가

--- src/components/sidBarMenu/sideBarMenuInfo/sideBarMenuInfoReview/createReview/createReview.tsx
+++ src/components/sidBarMenu/sideBarMenuInfo/sideBarMenuInfoReview/createReview/createReview.tsx
@@ -8,16 +8,30 @@ import {
   createReviewInputImg,
   createReviewInputImgIcon,
   createReviewInputImgLabel,
+  createReviewInputImgLabelImg,
+  createReviewInputImgLabelWrapper,
   createReviewInputTitle,
   createReviewUnderLine,
 } from './createReview.css';
 import { FaCirclePlus } from 'react-icons/fa6';
 import { useRecoilState } from 'recoil';
 import { reviewState } from '../../../../../recoil/atoms/menuState';
+import { useState } from 'react';
 
 export const CreateReview = () => {
   const { register, handleSubmit } = useForm();
   const [review, setReview] = useRecoilState(reviewState);
+  const [image, setImage] = useState<string | null | ArrayBuffer>(null);
+  const handleImageChange = (e: any) => {
+    const file = e.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setImage(reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
   const onClickCancle = () => {
     setReview({ isOpened: false });
   };
@@ -28,14 +42,24 @@ export const CreateReview = () => {
   return (
     <div className={createReviewContainer}>
       <form className={createReviewForm} onSubmit={handleSubmit(onSubmit)}>
-        <span className={createReviewInputTitle}>리뷰 사진 (필수)</span>
-        <label htmlFor="imageUpload" className={createReviewInputImg}>
-          <div className={createReviewInputImgLabel}>
-            <FaCirclePlus className={createReviewInputImgIcon} />
-            <span>이미지 추가하기</span>
-          </div>
-        </label>
-        <input style={{ display: 'none' }} id="imageUpload" type="file" placeholder="이미지" {...register('name')} />
+        <span className={createReviewInputTitle}>리뷰 사진 (필수, 최대 1장)</span>
+        <div className={createReviewInputImgLabelWrapper}>
+          <label htmlFor="imageUpload" className={createReviewInputImg}>
+            <div className={createReviewInputImgLabel}>
+              <FaCirclePlus className={createReviewInputImgIcon} />
+              <span>이미지 추가하기</span>
+            </div>
+          </label>
+          {image && <img className={createReviewInputImgLabelImg} src={image as string} alt="리뷰 이미지" />}
+        </div>
+        <input
+          style={{ display: 'none' }}
+          id="imageUpload"
+          type="file"
+          placeholder="이미지"
+          {...register('name')}
+          onChange={handleImageChange}
+        />
         <div className={createReviewUnderLine} />
         <span className={createReviewInputTitle}>리뷰</span>
         <textarea className={createReviewInput} placeholder="리뷰 내용을 작성해주세요." {...register('content')} />
